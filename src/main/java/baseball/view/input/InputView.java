6d2e8c297f60906d7eb09d74d82dfb24666a9e1f
jdohun <wjdehgns97@naver.com>
2023-11-17 23:08:55 +0900

    feat (InputView): 숫자 야구에 사용하는 숫자를 입력하는 기능 구현

---
+++ src/main/java/baseball/view/input/InputView.java
@@ -0,0 +1,13 @@
+package baseball.view.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String INPUT_NUMBERS = "숫자를 입력해주세요 : ";
+    private static final String INPUT_RETRY = String.format("게임을 새로 시작하려면 %d, 종료하려면 %d를 입력하세요.", 1, 2);
+
+    public static String requestInputNumbers(){
+        System.out.printf(INPUT_NUMBERS);
+        return Console.readLine();
+    }
+}

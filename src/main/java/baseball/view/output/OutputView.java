0c559c808ebb7346f1bd78a54ca357a4e6ca47ae
DH Jeong <wjdehgns97@gmail.com>
2023-11-17 20:16:03 +0900

    feat (OutputView): 숫자 야구 시작 메시지를 출력하는 기능 구현

---
+++ src/main/java/baseball/view/output/OutputView.java
@@ -0,0 +1,9 @@
+package baseball.view.output;
+
+public class OutputView {
+    private static final String OUTPUT_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public static void printStart(){
+        System.out.println(OUTPUT_START_MESSAGE);
+    }
+}

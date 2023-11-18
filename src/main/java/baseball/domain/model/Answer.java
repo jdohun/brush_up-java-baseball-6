387c63b34da9a2948e72284c6281a0cf2fdbc73e
DH Jeong <wjdehgns97@gmail.com>
2023-11-18 17:46:23 +0900

    feat: Answer 끼리 비교하여 스트라이크의 개수와 볼 개수를 세는 기능 구현 및 에러메시지 리터럴 처리
    Answer.java
    countStrike()
    countBall()
    isBallCount()

--- src/main/java/baseball/domain/model/Answer.java
+++ src/main/java/baseball/domain/model/Answer.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
+import java.util.stream.IntStream;
 
 import static baseball.domain.model.AnswerConstant.*;
 
@@ -32,7 +33,7 @@ public class Answer {
 
     private static void validateSize(List<Integer> pendingValidationData) {
         if (pendingValidationData.size() != LIMIT_SIZE_OF_ANSWER) {
-            throw new IllegalArgumentException(ERROR_SIZE_LIMIT_EXCEEDED);
+            throw new IllegalArgumentException(LIMIT_SIZE_OF_ANSWER + "개의 숫자를 가져야 합니다.");
         }
     }
 
@@ -40,7 +41,7 @@ public class Answer {
         Set<Integer> duplicateChecker = new HashSet<>();
         for (int temp : pendingValidationData) {
             if (!duplicateChecker.add(temp)) {
-                throw new IllegalArgumentException(ERROR_HAS_DUPLICATE);
+                throw new IllegalArgumentException("중복된 숫자가 존재합니다.");
             }
         }
     }
@@ -48,7 +49,7 @@ public class Answer {
     private static void validateInput(String inputNumbers) {
         Matcher matcher = PATTERN_ANSWER.matcher(inputNumbers);
         if (!matcher.matches()) {
-            throw new IllegalArgumentException(ERROR_DOES_NOT_FIT_PATTERN);
+            throw new IllegalArgumentException("입력 형식에 맞지 않습니다.");
         }
     }
 
@@ -59,5 +60,23 @@ public class Answer {
             pendingValidationAnswer.add(Integer.parseInt(temp));
         }
         return pendingValidationAnswer;
+    }
+
+    private int countStrike(Answer target) {
+        return (int) IntStream.range(0, LIMIT_SIZE_OF_ANSWER)
+                .filter(i -> this.numbers.get(i) == target.numbers.get(i))
+                .count();
+    }
+
+    private int countBall(Answer target) {
+        return (int) IntStream.range(0, LIMIT_SIZE_OF_ANSWER)
+                .filter(i -> isBallCount(target.numbers.get(i), i))
+                .count();
+    }
+
+    private boolean isBallCount(int targetNumber, int excludedIndex) {
+        return IntStream.range(0, LIMIT_SIZE_OF_ANSWER)
+                .filter(index -> index != excludedIndex)
+                .anyMatch(index -> targetNumber == this.numbers.get(index));
     }
 }

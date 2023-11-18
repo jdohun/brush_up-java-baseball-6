6cb4187fa4a44d289540d26bcc8cf0549647bfe8
DH Jeong <wjdehgns97@gmail.com>
2023-11-19 05:48:37 +0900

    feat: 자동 단일 숫자 객체 생성 기능을 활용하여 자동으로 Answer 객체를 생성하는 기능 구현
    AnswerGenerator.java

--- src/main/java/baseball/generator/AnswerGenerator.java
+++ src/main/java/baseball/generator/AnswerGenerator.java
@@ -1,19 +1,32 @@
 package baseball.generator;
 
 import baseball.domain.model.Answer;
+import baseball.domain.model.SingleNumber;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 public class AnswerGenerator {
-    private static final AnswerGenerator ANSWER_GENERATOR = new AnswerGenerator();
-    private final ThreeRandomNumberGenerator THREE_RANDOM_NUMBER_GENERATOR = ThreeRandomNumberGenerator.getInstance();
+    private final SingleNumberGenerator ANSWER_NUMBER_GENERATOR = SingleNumberGenerator.getInstance();
 
     private AnswerGenerator() {
     }
 
     public static AnswerGenerator getInstance() {
-        return ANSWER_GENERATOR;
+        return Holder.ANSWER_GENERATOR;
     }
 
     public Answer run() {
-        return Answer.from(THREE_RANDOM_NUMBER_GENERATOR.run());
+        return Answer.from(createSingleNumbers());
+    }
+
+    public List<SingleNumber> createSingleNumbers() {
+        return Stream.iterate(ANSWER_NUMBER_GENERATOR.run(), singleNumber
+                        -> ANSWER_NUMBER_GENERATOR.run())
+                .distinct().toList();
+    }
+
+    public static class Holder {
+        private static final AnswerGenerator ANSWER_GENERATOR = new AnswerGenerator();
     }
 }

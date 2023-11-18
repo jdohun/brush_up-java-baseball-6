e097d75051381b05281a1641e589a9d21f06dc98
jdohun <wjdehgns97@naver.com>
2023-11-19 05:04:21 +0900

    feat: Answer 를 단일 숫자 클래스를 이용한 일급 컬렉션으로 수정 및 사용하는 상수를 내부로 이동
    Answer.java
    AnswerConstant.java 삭제

--- src/main/java/baseball/domain/model/Answer.java
+++ src/main/java/baseball/domain/model/Answer.java
@@ -1,23 +1,26 @@
 package baseball.domain.model;
 
+import baseball.dto.ComparisonResult;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
-import static baseball.domain.model.AnswerConstant.*;
-
 public class Answer {
+    public static final int LIMIT_SIZE_OF_ANSWER = 3;
+    private static final Pattern PATTERN_ANSWER = Pattern.compile("^[1-9][1-9][1-9]$");
+    private static final String INPUT_DELIMITER = "";
 
-    private List<Integer> numbers;
+    private List<SingleNumber> numbers;
 
-    private Answer(List<Integer> numbers) {
+    private Answer(List<SingleNumber> numbers) {
         this.numbers = numbers;
     }
 
-    public static Answer from(List<Integer> autoNumbers) {
+    public static Answer from(List<SingleNumber> autoNumbers) {
         validateSize(autoNumbers);
         validateDuplication(autoNumbers);
         return new Answer(autoNumbers);
@@ -25,24 +28,21 @@ public class Answer {
 
     public static Answer from(String inputNumbers) {
         validateInput(inputNumbers);
-        List<Integer> pendingValidationAnswer = inputToPendingValidationAnswer(inputNumbers);
-        validateSize(pendingValidationAnswer);
-        validateDuplication(pendingValidationAnswer);
-        return new Answer(pendingValidationAnswer);
+        List<SingleNumber> singleNumbers = getSingleNumberList(inputNumbers);
+        validateSize(singleNumbers);
+        validateDuplication(singleNumbers);
+        return new Answer(singleNumbers);
     }
 
-    private static void validateSize(List<Integer> pendingValidationData) {
-        if (pendingValidationData.size() != LIMIT_SIZE_OF_ANSWER) {
+    private static void validateSize(List<SingleNumber> singleNumbers) {
+        if (singleNumbers.size() != LIMIT_SIZE_OF_ANSWER) {
             throw new IllegalArgumentException(LIMIT_SIZE_OF_ANSWER + "개의 숫자를 가져야 합니다.");
         }
     }
 
-    private static void validateDuplication(List<Integer> pendingValidationData) {
-        Set<Integer> duplicateChecker = new HashSet<>();
-        for (int temp : pendingValidationData) {
-            if (!duplicateChecker.add(temp)) {
-                throw new IllegalArgumentException("중복된 숫자가 존재합니다.");
-            }
+    private static void validateDuplication(List<SingleNumber> singleNumbers) {
+        if (new HashSet<>(singleNumbers).size() != LIMIT_SIZE_OF_ANSWER) {
+            throw new IllegalArgumentException("중복된 숫자가 존재합니다.");
         }
     }
 
@@ -53,13 +53,30 @@ public class Answer {
         }
     }
 
-    private static List<Integer> inputToPendingValidationAnswer(String inputNumbers) {
+    private static List<SingleNumber> getSingleNumberList(String inputNumbers) {
+        List<Integer> integers = getIntegerList(inputNumbers);
+        List<SingleNumber> singleNumbers = new ArrayList<>();
+        for (int temp : integers) {
+            singleNumbers.add(SingleNumber.from(temp));
+        }
+        return singleNumbers;
+    }
+
+    private static List<Integer> getIntegerList(String inputNumbers) {
         List<Integer> pendingValidationAnswer = new ArrayList<>();
 
         for (String temp : inputNumbers.split(INPUT_DELIMITER)) {
             pendingValidationAnswer.add(Integer.parseInt(temp));
         }
         return pendingValidationAnswer;
+    }
+
+    public ComparisonResult compareTo(Answer target) {
+        int countOfBall = countBall(target);
+        int countOfStrike = countStrike(target);
+        boolean isCorrect = countOfStrike == LIMIT_SIZE_OF_ANSWER;
+
+        return new ComparisonResult(countOfBall, countOfStrike, isCorrect);
     }
 
     private int countStrike(Answer target) {
@@ -74,7 +91,7 @@ public class Answer {
                 .count();
     }
 
-    private boolean isBallCount(int targetNumber, int excludedIndex) {
+    private boolean isBallCount(SingleNumber targetNumber, int excludedIndex) {
         return IntStream.range(0, LIMIT_SIZE_OF_ANSWER)
                 .filter(index -> index != excludedIndex)
                 .anyMatch(index -> targetNumber == this.numbers.get(index));

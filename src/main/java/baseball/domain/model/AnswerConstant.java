cc7d3895f3232ec72f8a7cd43aa0a0810c354375
DH Jeong <wjdehgns97@gmail.com>
2023-11-18 17:47:10 +0900

    refactor: 리터럴 처리된 에러메시지 상수 제거
    AnswerConstant.java

--- src/main/java/baseball/domain/model/AnswerConstant.java
+++ src/main/java/baseball/domain/model/AnswerConstant.java
@@ -9,7 +9,4 @@ final class AnswerConstant {
     static final Pattern PATTERN_ANSWER = Pattern.compile("^[1-9][1-9][1-9]$");
     static final String INPUT_DELIMITER = "";
     static final int LIMIT_SIZE_OF_ANSWER = 3;
-    static final String ERROR_HAS_DUPLICATE = "중복된 숫자가 존재합니다.";
-    static final String ERROR_SIZE_LIMIT_EXCEEDED = String.format("%d개의 숫자를 가져야 합니다.", LIMIT_SIZE_OF_ANSWER);
-    static final String ERROR_DOES_NOT_FIT_PATTERN = "입력 형식에 맞지 않습니다.";
 }

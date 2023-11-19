588ff78adf3369a84a0fc663cbb969f52468b1ee
DH Jeong <wjdehgns97@gmail.com>
2023-11-20 02:54:21 +0900

    feat: 게임 종료 안내문을 출력하는 기능 구현
    OutputView.java

--- src/main/java/baseball/view/output/OutputView.java
+++ src/main/java/baseball/view/output/OutputView.java
@@ -1,9 +1,11 @@
 package baseball.view.output;
 
+import baseball.domain.model.Answer;
 import baseball.dto.ComparisonResult;
 
 public class OutputView {
     private static final String OUTPUT_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String OUTPUT_GAME_OVER = String.format("%d개의 숫자를 모두 맞히셨습니다! 게임 종료", Answer.LIMIT_SIZE_OF_ANSWER);
 
     private OutputView() {
     }
@@ -17,7 +19,11 @@ public class OutputView {
     }
 
     public void printResult(ComparisonResult result) {
-        System.out.println(ResultFormat.getFormatByResult(result));;
+        System.out.println(ResultFormat.getFormatByResult(result));
+    }
+
+    public void printGameOver(){
+        System.out.println(OUTPUT_GAME_OVER);
     }
 
     private static class OutputViewHolder {

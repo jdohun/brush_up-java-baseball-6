c3582b089e6fbac2265730dcc145595958463cc6
DH Jeong <wjdehgns97@gmail.com>
2023-11-20 02:52:58 +0900

    feat: 재시작 여부를 입력받는 기능 구현
    InputView.java

--- src/main/java/baseball/view/input/InputView.java
+++ src/main/java/baseball/view/input/InputView.java
@@ -4,7 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_NUMBERS = "숫자를 입력해주세요 : ";
-    private static final String INPUT_RETRY = String.format("게임을 새로 시작하려면 %d, 종료하려면 %d를 입력하세요.", 1, 2);
+    private static final int RESTART = 1;
+    private static final int GAME_OVER = 2;
+    private static final String INPUT_RETRY = String.format("게임을 새로 시작하려면 %d, 종료하려면 %d를 입력하세요.", RESTART, GAME_OVER);
 
     private InputView() {
     }
@@ -15,6 +17,11 @@ public class InputView {
 
     public String requestInputNumbers() {
         System.out.printf(INPUT_NUMBERS);
+        return Console.readLine();
+    }
+
+    public String requestInputIsRestart() {
+        System.out.println(INPUT_RETRY);
         return Console.readLine();
     }
 

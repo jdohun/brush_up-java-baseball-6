841ce18b8c27c5a5f78ab697a3f634710b4c60ef
jdohun <wjdehgns97@naver.com>
2023-11-19 21:32:30 +0900

    feat: 자동 Answer 객체 생성시 주입 받는 List  제한 추가
    AnswerGenerator.java
    SingleNumberGenerator.java

--- src/main/java/baseball/generator/SingleNumberGenerator.java
+++ src/main/java/baseball/generator/SingleNumberGenerator.java
@@ -1,16 +1,9 @@
 package baseball.generator;
 
-import baseball.domain.model.Answer;
 import baseball.domain.model.SingleNumber;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 public class SingleNumberGenerator {
-    private static final int COUNT_OF_NUMBERS = Answer.LIMIT_SIZE_OF_ANSWER;
     private static final int START_OF_RANGE = SingleNumber.START_OF_RANGE;
     private static final int END_OF_RANGE = SingleNumber.END_OF_RANGE;
 

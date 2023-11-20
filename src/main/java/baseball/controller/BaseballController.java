9e8e5c46467f7207574782b0bc8edca6af018854
DH Jeong <wjdehgns97@gmail.com>
2023-11-21 01:41:28 +0900

    refactor: 메서드 이름이 역할을 잘 드러내도록 수정
    InputHandler.java
    BaseballController.java
    inputToIntegerArray() -> inputToSourceForAnswer()

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -27,7 +27,7 @@ public class BaseballController {
         computer = ANSWER_GENERATOR.run();
 
         do {
-            Answer userAnswer = Answer.from(InputHandler.inputToIntegerArray(INPUT_VIEW.inputNumbers()));
+            Answer userAnswer = Answer.from(InputHandler.inputToSourceForAnswer(INPUT_VIEW.inputNumbers()));
             comparisonResult = computer.compareTo(userAnswer);
             OUTPUT_VIEW.printResult(comparisonResult);
         } while (Answer.LIMIT_SIZE_OF_ANSWER != comparisonResult.countOfStrike());
